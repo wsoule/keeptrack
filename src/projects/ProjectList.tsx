860cc36ae784c161474883fe96f74371ff816c49
Wyat Soule <wsoule679@gmail.com>
2022-10-10 17:55:50 -0400

    Format the list data as list items, format the list data as cards

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
 import { Project } from './Project';
 
 interface ProjectListProps {
-	projects: Project[];
+  projects: Project[];
 }
 
-function ProjectList( { projects }: ProjectListProps) : JSX.Element {
-  return <pre>{JSON.stringify(projects, null, ' ')}</pre>;
+function ProjectList({ projects }: ProjectListProps): JSX.Element {
+  return (
+    <div className="row">
+      {projects.map((project) => (
+        <div key={project.id} className="cols-sm">
+          <div className="card">
+            <img src={project.imageUrl} alt={project.name} />
+            <section className="section dark">
+              <h5 className="strong">
+                <strong>{project.name}</strong>
+              </h5>
+              <p>{project.description}</p>
+              <p>Budget : {project.budget.toLocaleString()}</p>
+            </section>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default ProjectList;
