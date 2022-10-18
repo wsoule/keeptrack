38dd4806cfa3e9bde55caded7ccf9f0ec27ada04
Wyat Soule <wsoule679@gmail.com>
2022-10-17 22:03:28 -0400

    edit clock file and test webhook

--- src/add-minutes/clock.tsx
+++ src/add-minutes/clock.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function addMinutes(date: Date, minutes: number): Date {
+function changeMinutes(date: Date, minutes: number): Date {
   // multiply minutes by 6000 is to convert minutes to milliseconds
   return new Date(date.getTime() + minutes * 60000);
 }
@@ -9,18 +9,20 @@ export function Clock(): JSX.Element {
   const [time, setTime] = React.useState(new Date());
 
   const handleClick1 = (): void => {
-    setTime(addMinutes(time, 10));
-    setTime(addMinutes(time, 10));
+    setTime(changeMinutes(time, 10));
   };
   const handleClick2 = (): void => {
-    setTime((previousTime) => addMinutes(previousTime, 10));
-    setTime((previousTime) => addMinutes(previousTime, 10));
+    setTime((previousState) => changeMinutes(previousState, -10));
+  };
+  const refresh = (): void => {
+    setTime(new Date());
   };
   return (
     <div>
       <p>{time.toLocaleString()}</p>
+      <button onClick={handleClick2}>- 10 Minutes</button>
+      <button onClick={refresh}>Refresh</button>
       <button onClick={handleClick1}>+ 10 Minutes</button>
-      <button onClick={handleClick2}>+ 10 Minutes2</button>
     </div>
   );
 }
